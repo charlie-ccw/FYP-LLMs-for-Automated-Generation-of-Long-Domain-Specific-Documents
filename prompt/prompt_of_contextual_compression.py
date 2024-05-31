fea1d5db7bbcae1c8fbe9963955aa7dba6bc53f8
charlie-ccw <2392687133@qq.com>
2024-05-31 04:17:39 +0100

    fix the format of contextual compression prompt

--- prompt/prompt_of_contextual_compression.py
+++ prompt/prompt_of_contextual_compression.py
@@ -1,6 +1,6 @@
 CONTEXTUAL_COMPRESSION_SYSTEM = "You are an excellent contextual extractor"
 
-CONTEXTUAL_COMPRESSION_PROMPT = """Given the following question and context, extract any part of the context *AS IS* that is relevant to answer the question. If none of the context is relevant return {no_output_str}. 
+CONTEXTUAL_COMPRESSION_PROMPT = """Given the following question and context, extract any part of the context *AS IS* that is relevant to answer the question. If none of the context is relevant return "NO OUTPUT STRING". 
 
 Remember, *DO NOT* edit the extracted parts of the context.
 
