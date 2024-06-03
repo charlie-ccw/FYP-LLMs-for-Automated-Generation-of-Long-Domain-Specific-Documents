8e8a68bfb81e127897649276033b9e90913a4524
charlie-ccw <2392687133@qq.com>
2024-06-03 04:08:22 +0100

    Force the LLM of Agent to be gpt-4o

--- agent/key_info_retrieval_agent.py
+++ agent/key_info_retrieval_agent.py
@@ -12,7 +12,7 @@ def get_key_info_retrieval_agent(temperature: float = 0.5):
 
     # Choose the LLM that will drive the agent
     # Only certain models support this
-    llm = ChatOpenAI(model=MODEL, temperature=temperature)
+    llm = ChatOpenAI(model='gpt-4o', temperature=temperature)
     tools = [RetrievalQAWithLLMAndResortTool()]
     # Construct the OpenAI Tools agent
     agent = create_openai_tools_agent(llm, tools, prompt)
