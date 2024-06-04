3079b0e1efc119359443c2190941101ebcbf3316
charlie-ccw <2392687133@qq.com>
2024-06-04 18:27:23 +0100

    update more evaluation matrixs and evaluation process

--- evaluation.py
+++ evaluation.py
@@ -5,12 +5,15 @@ such as BLEU, ROUGE, etc.
 Note:
     You need to manually adjust the version you want to evaluate in main() program.
 """
+import asyncio
 import json
-from util.evaluation_matrix import calculate_BLEU, calculate_METEOR, calculate_ROUGE_L, calculate_BLEURT, \
-    calculate_GPT_SELF_EVALUATION
+import os.path
+
+from util.evaluation_matrix import calculate_BLEU, calculate_METEOR, calculate_METEOR_of_summary, calculate_ROUGE, \
+    calculate_Embedding_similarity_of_consine, calculate_GPT_SELF_EVALUATION
 
 
-def evaluate_generated_file(file_name_without_extension: str, generation_version: int):
+async def evaluate_generated_file(file_name_without_extension: str, generation_version: str):
     print(f">>>>>>>>>>>>>>> {file_name_without_extension} <<<<<<<<<<<<<<< Evaluation")
     # Set the File Path for generation, target and template file
     template_file_path = 'project_template/template_version_1.json'
@@ -29,21 +32,28 @@ def evaluate_generated_file(file_name_without_extension: str, generation_version
 
     # Initialise the variables for storing evaluation scores
     evaluation_matrix = {}
+
     bleu_result = {}
-    rouge_l_result = {}
     meteor_result = {}
-    bleurt_result = {}
+    meteor_of_summary_result = {}
+    rouge_1_result = {}
+    rouge_l_result = {}
+    embedding_similarity_of_cosine_result = {}
     gpt_self_evaluation_result = {}
+
     bleu_total = 0
-    rouge_l_total = 0
     meteor_total = 0
-    bleurt_total = 0
+    meteor_of_summary_total = 0
+    rouge_1_total = 0
+    rouge_l_total = 0
+    embedding_similarity_of_cosine_total = 0
     gpt_self_evaluation_total = 0
 
     # Start iteration and get all evaluation score to store
     for section_id, section_data in generated_sections.items():
         # Check the section name
-        if (section_id in target_sections.keys()) and (section_data["section_name"].lower() == target_sections[section_id]["section_name"].lower()):
+        if (section_id in target_sections.keys()) and (
+                section_data["section_name"].lower() == target_sections[section_id]["section_name"].lower()):
             # Get the generation and target text of current section
             generated_data = section_data["generation"]
             target_data = target_sections[section_id]["section_info"]
@@ -52,44 +62,67 @@ def evaluate_generated_file(file_name_without_extension: str, generation_version
                 'description']
 
             # Call functions to calculate evaluation scores
-            bleu_score = calculate_BLEU(generation_text=generated_data, target_text=target_data)
-            rouge_l_score = calculate_ROUGE_L(generation_text=generated_data, target_text=target_data)
-            meteor_score = calculate_METEOR(generation_text=generated_data, target_text=target_data)
-            bleurt_score = calculate_BLEURT(generation_text=generated_data, target_text=target_data)
-            gpt_self_evaluation_score = calculate_GPT_SELF_EVALUATION(template_requirement=template_requirement,
-                                                                      generation_text=generated_data,
-                                                                      target_text=target_data)
-            # Store these evaluation scores
-            bleu_result[section_id] = bleu_score
-            rouge_l_result[section_id] = rouge_l_score
-            meteor_result[section_id] = meteor_score
-            bleurt_result[section_id] = bleurt_score
-            gpt_self_evaluation_result[section_id] = gpt_self_evaluation_score
+            while 1:
+                try:
+                    bleu_score = calculate_BLEU(generation_text=generated_data, target_text=target_data)
+                    meteor_score = calculate_METEOR(generation_text=generated_data, target_text=target_data)
+                    meteor_of_summary_score = await calculate_METEOR_of_summary(generation_text=generated_data,
+                                                                                target_text=target_data)
+                    rouge_1_score = calculate_ROUGE(generation_text=generated_data, target_text=target_data)['rouge-1']['f']
+                    rouge_l_score = calculate_ROUGE(generation_text=generated_data, target_text=target_data)['rouge-l']['f']
+                    embedding_similarity_of_cosine_score = calculate_Embedding_similarity_of_consine(
+                        generation_text=generated_data,
+                        target_text=target_data)
+                    gpt_self_evaluation_score = await calculate_GPT_SELF_EVALUATION(template_requirement=template_requirement,
+                                                                                    generation_text=generated_data,
+                                                                                    target_text=target_data)
+                    # Store these evaluation scores
+                    bleu_result[section_id] = bleu_score
+                    meteor_result[section_id] = meteor_score
+                    meteor_of_summary_result[section_id] = meteor_of_summary_score
+                    rouge_1_result[section_id] = rouge_1_score
+                    rouge_l_result[section_id] = rouge_l_score
+                    embedding_similarity_of_cosine_result[section_id] = embedding_similarity_of_cosine_score
+                    gpt_self_evaluation_result[section_id] = gpt_self_evaluation_score
 
-            # calculate the sum of each score for calculating final score of whole file
-            bleu_total += bleu_score
-            rouge_l_total += rouge_l_score
-            meteor_total += meteor_score
-            bleurt_total += bleurt_score
-            gpt_self_evaluation_total += gpt_self_evaluation_score['score']
+                    # calculate the sum of each score for calculating final score of whole file
+                    bleu_total += bleu_score
+                    meteor_total += meteor_score
+                    meteor_of_summary_total += meteor_of_summary_score
+                    rouge_1_total += rouge_1_score
+                    rouge_l_total += rouge_l_score
+                    embedding_similarity_of_cosine_total += embedding_similarity_of_cosine_score
+                    gpt_self_evaluation_total += gpt_self_evaluation_score['score']
+                    break
+                except Exception as e:
+                    print(f'ERROR----------{file_name_without_extension}----------{section_id}----------{e}')
+                    break
 
         else:
             # Skip the section if section structure doesn't fit
-            print(f"The ROUGE Scores of {section_id} is: None")
+            print(f"The Evaluation of {file_name_without_extension} --- {section_id} is: None")
 
     # calculate the final score for the whole file for each evaluation matrix
     bleu_result['final'] = bleu_total / len(list(bleu_result.keys()))
-    rouge_l_result['final'] = rouge_l_total / len(list(rouge_l_result.keys()))
     meteor_result['final'] = meteor_total / len(list(meteor_result.keys()))
-    bleurt_result['final'] = bleurt_total / len(list(bleurt_result.keys()))
+    meteor_of_summary_result['final'] = meteor_of_summary_total / len(list(meteor_of_summary_result.keys()))
+    rouge_1_result['final'] = rouge_1_total / len(list(rouge_1_result.keys()))
+    rouge_l_result['final'] = rouge_l_total / len(list(rouge_l_result.keys()))
+    embedding_similarity_of_cosine_result['final'] = embedding_similarity_of_cosine_total / len(
+        list(embedding_similarity_of_cosine_result.keys()))
     gpt_self_evaluation_result['final'] = gpt_self_evaluation_total / len(list(gpt_self_evaluation_result.keys()))
 
     # Combine all evaluation matrixs together
     evaluation_matrix['BLEU'] = bleu_result
-    evaluation_matrix['ROUGE_L'] = rouge_l_result
     evaluation_matrix['METEOR'] = meteor_result
-    evaluation_matrix['BLEURT'] = bleurt_result
+    evaluation_matrix['METEOR_OF_SUMMARY'] = meteor_of_summary_result
+    evaluation_matrix['ROUGE_1'] = rouge_1_result
+    evaluation_matrix['ROUGE_L'] = rouge_l_result
+    evaluation_matrix['EMBEDDING_SIMILARITY_OF_COSINE'] = embedding_similarity_of_cosine_result
     evaluation_matrix['GPT_SELF_EVALUATION'] = gpt_self_evaluation_result
+
+    if not os.path.isdir(f"evaluation_result/version_{generation_version}"):
+        os.mkdir(f"evaluation_result/version_{generation_version}")
 
     # Save scores
     with open(f"evaluation_result/version_{generation_version}/{file_name_without_extension}.json", 'w',
@@ -97,10 +130,7 @@ def evaluate_generated_file(file_name_without_extension: str, generation_version
         json.dump(evaluation_matrix, f, indent=4)
 
 
-def main():
-    # Set the generated version you want to evvaluate
-    generation_version = 1
-
+async def main():
     # Load the train and test files with summary
     train_test_file_path = "project_template/template_version_1_summary.json"
     with open(train_test_file_path, 'r', encoding='utf-8') as f:
@@ -109,12 +139,29 @@ def main():
     # Get the test files with summary
     test_datasets = train_test_datasets['test']
 
-    # Start iteration and evaluate each generation of specific version
-    for idx, test_dataset in enumerate(test_datasets):
-        file_name_without_extension = test_dataset['file_name']
-        evaluate_generated_file(file_name_without_extension=file_name_without_extension,
-                                generation_version=generation_version)
+    # Set the generated version you want to evvaluate
+    generation_versions = ['1', '2', '4', '5', '6', '3_1', '3_2', '3_4', '3_5', '3_6']
+
+    for generation_version in generation_versions:
+        print(
+            f"============================================================ {generation_version} ============================================================")
+        tasks = []
+        already_done_files = os.listdir(f"evaluation_result/version_{generation_version}")
+        # Start iteration and evaluate each generation of specific version
+        for idx, test_dataset in enumerate(test_datasets):
+            file_name_without_extension = test_dataset['file_name']
+            if file_name_without_extension + '.json' not in already_done_files:
+                tasks.append(evaluate_generated_file(file_name_without_extension=file_name_without_extension,
+                                                     generation_version=generation_version))
+
+            if len(tasks) == 25:
+                await asyncio.gather(*tasks)
+                tasks = []
+
+        # Process any remaining files
+        if tasks:
+            await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- util/evaluation_matrix.py
+++ util/evaluation_matrix.py
@@ -1,15 +1,18 @@
+import asyncio
+import os
+import numpy as np
 import nltk
 from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_openai import OpenAIEmbeddings
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from nltk.translate import meteor_score
 from nltk.tokenize import word_tokenize
 from rouge import Rouge
-import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from prompt.prompt_of_gpt_self_evaluation import GPT_SELF_EVALUATION_SYSTEM, GPT_SELF_EVALUATION_PROMPT
-from util.prompt_based_generation import prompt_based_generation
+from util.prompt_based_generation import prompt_based_generation, aprompt_based_generation
 
 nltk.download("wordnet")
 nltk.download('omw-1.4')
@@ -17,6 +20,15 @@ nltk.download('punkt')
 
 tokenizer = AutoTokenizer.from_pretrained("Elron/bleurt-base-512")
 model = AutoModelForSequenceClassification.from_pretrained("Elron/bleurt-base-512")
+
+
+def cosine_similarity(embeddings1, embeddings2):
+    dot_product = np.dot(embeddings1, embeddings2)
+    norm_emb1 = np.linalg.norm(embeddings1)
+    norm_emb2 = np.linalg.norm(embeddings2)
+    similarity = dot_product / (norm_emb1 * norm_emb2)
+
+    return similarity
 
 
 def calculate_BLEU(generation_text: str, target_text: str) -> float:
@@ -55,7 +67,30 @@ def calculate_METEOR(generation_text: str, target_text: str) -> float:
     return float(meteor_result)
 
 
-def calculate_ROUGE_L(generation_text: str, target_text: str) -> float:
+async def calculate_METEOR_of_summary(generation_text: str, target_text: str) -> float:
+    """
+    METEOR evaluates the similarity between two texts, considering synonyms, roots, and suffixes.
+    However, it is sensitive to the order of generated sentences,
+    which can lead to a lower score for sentences that have the same meaning but different word orders.
+    :param generation_text: The text you want to evaluate
+    :param target_text: Your target example
+    :return: METEOR score
+    """
+    tasks = [
+        aprompt_based_generation(prompt=f"please summarize the following section context:\n{generation_text}", model='gpt-4o', temperature=0),
+        aprompt_based_generation(prompt=f"please summarize the following section context:\n{target_text}", model='gpt-4o', temperature=0)
+    ]
+    response = await asyncio.gather(*tasks)
+
+    reference_tokens = [word_tokenize(response[1].content)]
+    candidate_tokens = word_tokenize(response[0].content)
+
+    meteor_result = meteor_score.meteor_score(reference_tokens, candidate_tokens)
+
+    return float(meteor_result)
+
+
+def calculate_ROUGE(generation_text: str, target_text: str) -> dict:
     """
     Evaluating text similarity using the longest common subsequence is suitable for long text generation tasks.
     It considers both recall and precision.
@@ -64,14 +99,14 @@ def calculate_ROUGE_L(generation_text: str, target_text: str) -> float:
     :param target_text: Your target example
     :return: ROUGE-L score
     """
-    rouge = Rouge(metrics=['rouge-l'])
+    rouge = Rouge(metrics=['rouge-l', 'rouge-1'])
 
-    rouge_l_result = rouge.get_scores(generation_text, target_text, avg=True)['rouge-l']['r']
+    rouge_result = rouge.get_scores(generation_text, target_text, avg=True)
 
-    return float(rouge_l_result)
+    return rouge_result
 
 
-def calculate_BLEURT(generation_text: str, target_text: str) -> float:
+def calculate_Embedding_similarity_of_consine(generation_text: str, target_text: str) -> float:
     """
     Based on the transformer model, it can support contextual understanding and
     provide more accurate semantic-level judgments.
@@ -81,25 +116,26 @@ def calculate_BLEURT(generation_text: str, target_text: str) -> float:
     :param target_text: Your target example
     :return: BLEURT score
     """
-    inputs = tokenizer(target_text, generation_text, return_tensors='pt', padding=True, truncation=True, max_length=512)
+    embeddings_model = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = embeddings_model.embed_documents(
+        [
+            generation_text,
+            target_text
+        ]
+    )
+    embedding_similarity_of_cosine = cosine_similarity(embeddings[0], embeddings[1])
 
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    bleurt_result = outputs.logits.squeeze().cpu().numpy()
-
-    return float(bleurt_result)
+    return float(embedding_similarity_of_cosine)
 
 
-def calculate_GPT_SELF_EVALUATION(generation_text: str, target_text: str,
-                                  template_requirement: str, model_name: str = 'gpt-4o') -> dict:
+async def calculate_GPT_SELF_EVALUATION(generation_text: str, target_text: str,
+                                  template_requirement: str) -> dict:
     """
     By leveraging the model's discriminative capabilities, this tool evaluates the generated text,
     providing both the reasoning and corresponding scores.
     :param generation_text: The text you want to evaluate
     :param target_text: Your target example
     :param template_requirement: The template for your generated text
-    :param model_name: The model you want to use for exaluation
     :return: GPT-SELF-EVALUATION score
     """
     chat_template = ChatPromptTemplate.from_messages(
@@ -112,7 +148,12 @@ def calculate_GPT_SELF_EVALUATION(generation_text: str, target_text: str,
     messages = chat_template.format_messages(template_requirement=template_requirement,
                                              generation_text=generation_text, target_text=target_text)
 
-    response = prompt_based_generation(prompt=messages, model=model_name, temperature=0.5, json_format=True)
+    while 1:
+        try:
+            response = await aprompt_based_generation(prompt=messages, model='gpt-4o', temperature=0.5, json_format=True)
+            break
+        except Exception as e:
+            print(e)
 
     gpt_self_evaluation_result = {
         'reason': response['reason'],
